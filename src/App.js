59daa24f41b30ae49c2e0e1cd5fb8082f786538d
PervertKun <shivenderkumar761@gmail.com>
2021-01-16 16:35:34 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,103 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      input: "",
-    };
+let items = [];
+export default function App() {
+  const [input, setinput] = useState("");
 
-    this.handleChange = this.handleChange.bind(this);
-    this.addItems = this.addItems.bind(this);
-    this.deleteItems = this.deleteItems.bind(this);
-    this.clearAll = this.clearAll.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      input: event.target.value,
-    });
-  }
-  addItems(event) {
-    event.preventDefault();
-    if (this.state.input !== "") {
-      if (this.state.items.includes(this.state.input)) {
+  const handleChange = (event) => {
+    setinput(event.target.value);
+  };
+  const addItems = () => {
+    if (input !== "") {
+      if (items.includes(input)) {
         alert("Task Already exists.");
       } else {
-        this.state.items.push(this.state.input);
-        console.log(this.state.items);
+        items.push(input);
       }
     }
-    this.setState({
-      input: "",
-    });
-  }
+    setinput("");
+  };
 
-  deleteItems(e) {
-    // alert("Deleting task - " + e);
-    var newList = this.state.items.filter((_item) => {
-      return _item !== e;
-    });
-    this.setState({
-      items: newList,
-    });
-  }
+  useEffect(() => {
+    const deleteItems = (i) => {
+      items = items.filter((item) => {
+        return item !== i;
+      });
+      return items;
+    };
+  }, []);
 
-  clearAll() {
-    if (this.state.items.length > 0) {
+  const clearAll = () => {
+    if (items.length > 0) {
       if (window.confirm("Clear All Tasks")) {
-        this.setState({
-          items: [],
-        });
-      } else { }
-      alert('Clearing all task Canceled')
+        items = [];
+      } else {
+        alert("Clearing all task Canceled");
+      }
     } else {
-      alert('There are no tasks to clear')
+      alert("There are no tasks to clear");
     }
-  }
+  };
 
-  render() {
-    const list = this.state.items.map((i) => (
-      <li key={i + 1}>
-        {i}
-        <button id="delBtn" onClick={this.deleteItems.bind(this, i)}>
-          Delete
-        </button>
-      </li>
-    ));
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 id="title">ToDo List</h1>
-        </header>
-        <div className="formApp">
-          <form id="todoForm" onSubmit={this.additems}>
-            <div className="inputArea">
-              <input
-                type="text"
-                placeholder="Enter The Task"
-                id="inputArea"
-                value={this.state.input}
-                onChange={this.handleChange}
-                maxLength="45"
-              />
-              <button id="addBtn" onClick={this.addItems}>
-                Add
-              </button>
-            </div>
-          </form>
-          <div className="itemsList">
-            <ul className='smooth-scroll'>{list}</ul>
-            <button id="clearAll" onClick={this.clearAll}>
-              Clear Tasks
-            </button>
-          </div>
+  const list = items.map((i, index) => (
+    <li key={index}>
+      {i}
+      <button id="delBtn" onClick={() => deleteItems(i)}>
+        Delete
+      </button>
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 id="title">ToDo List</h1>
+      </header>
+      <div className="formApp">
+        <div className="inputArea">
+          <input
+            type="text"
+            placeholder="Enter The Task"
+            id="inputArea"
+            value={input}
+            onChange={handleChange}
+            maxLength="45"
+          />
+          <button id="addBtn" onClick={addItems}>
+            Add
+          </button>
+        </div>
+        <div className="itemsList">
+          <ul className="smooth-scroll">{list}</ul>
+          <button id="clearAll" onClick={clearAll}>
+            Clear Tasks
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
