55b7b48a7bd6656428391471d3b88d6c4d31e357
PervertKun <shivenderkumar761@gmail.com>
2021-01-18 17:57:36 +0530

    g

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ export default function App() {
     }
     setinput("");
     setTime("");
+    refresh();
   };
   const clearAll = () => {
     if (items.length > 0) {
@@ -53,6 +54,7 @@ export default function App() {
         alert("Clearing all task Canceled");
       }
     }
+    refresh();
   };
   const list =
     items &&
@@ -80,6 +82,7 @@ export default function App() {
         return item !== itemTime[index];
       })
     );
+    refresh();
   };
 
   return (
