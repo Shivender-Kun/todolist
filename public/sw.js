b3b013ca814f84e483f2564c94bc9ab988c22ee5
PervertKun <shivenderkumar761@gmail.com>
2021-01-19 14:57:51 +0530

    create, refresh, update

--- public/sw.js
+++ public/sw.js
@@ -47,19 +47,25 @@ this.addEventListener("activate", (e) => {
 });
 
 this.addEventListener("fetch", (e) => {
-  // if (!navigator.onLine) {
-  e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        // Make clone f response
-        const resClone = res.clone();
-        caches.open(cacheVersion).then((cache) => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch((err) => caches.match(e.request).then((res) => res))
-  );
-  // }
+  if (navigator.onLine) {
+    e.waitUntil(
+      fetch(e.request)
+        .then((res) => {
+          // Make clone f response
+          const resClone = res.clone();
+          caches.open(cacheVersion).then((cache) => {
+            // Add response to cache
+            cache.put(e.request, resClone);
+          });
+          return res;
+        })
+        .catch((err) => caches.match(e.request).then((res) => res))
+    );
+  } else {
+    e.respondWith(
+      fetch(e.request)
+        .then((res) => res)
+        .catch((err) => caches.match(e.request).then((res) => res))
+    );
+  }
 });
